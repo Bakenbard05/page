a75dded4a804375765bd32f236c3f534297100f1
Bakenbard05 <83641135+Bakenbard05@users.noreply.github.com>
2024-12-04 18:49:11 +0400

    new mod

---
+++ mod 2/1/scripts.js
@@ -0,0 +1,10 @@
+let n = prompt("Введите число n:");
+
+let count = 1;
+
+for(let i = 2; i <= n / 2; i++){
+    if(n % i === 0) count++;
+
+}
+if(count < 2) alert("Число n простое");
+else alert("Число n не простое");
---
+++ mod 2/2/scripts.js
@@ -0,0 +1,14 @@
+function mult2of3(a, b, c){
+    ar = [a, b, c];
+    console.log(ar);
+    m1 = Math.max(...ar);
+    ar.pop(ar.findIndex(value => m1));
+    m2 = Math.max(...ar);
+    return m1 * m2;
+}
+
+a = Number(prompt("Число a: "));
+b = Number(prompt("Число b: "));
+c = Number(prompt("Число c: "));
+
+alert("Произведение двух максимальных чисел: " + String(mult2of3(a, b, c)));
